b14381b70c4ab3fdefe3c4b29744dfe930cd09ca
flandolfi <fran.landolfi@gmail.com>
2017-07-09 13:01:22 +0200

    Change Relation abstract class and all its subclasses.

--- src/DSLSet.java
+++ src/DSLSet.java
@@ -4,10 +4,13 @@ import java.util.HashSet;
 public class DSLSet {
     private final HashSet<Value> values = new HashSet<>();
     private final HashMap<DSLSet, Relation> relations = new HashMap<>();
+    private final String label;
 
+    public DSLSet(String label) { this.label = label; }
     public void addValue(Value value) { values.add(value); }
     public boolean contains(Value value) { return values.contains(value); }
     public HashSet<Value> getValues() { return new HashSet<>(values); }
+    public String getLabel() { return label; }
 
     public boolean addRelation(DSLSet range, Relation relation) {
         return relations.putIfAbsent(range, relation) != null;
@@ -16,11 +19,8 @@ public class DSLSet {
     public Relation getRelationWith(DSLSet range) {
         return relations.getOrDefault(range, new Relation(this, range) {
             @Override
-            public boolean addPair(Value lValue, Value rValue) { return true; }
-
-            @Override
-            public HashSet<Value> getAfterSet(Value value) {
-                return range.getValues(); // Cartesian product
+            public HashSet<Value> getUnconstrainedValues(Value value) {
+                return range.getValues(); // Cartesian Product
             }
         });
     }
--- src/Equality.java
+++ src/Equality.java
@@ -4,19 +4,7 @@ public class Equality extends Relation {
     public Equality(DSLSet domain, DSLSet range) { super(domain, range); }
 
     @Override
-    public boolean addPair(Value lValue, Value rValue) {
-        if (!domain.contains(lValue) || !range.contains(rValue))
-            return false;
-
-        afterSets.computeIfAbsent(lValue, k -> new HashSet<>()).add(rValue);
-        return true;
-    }
-
-    @Override
-    public HashSet<Value> getAfterSet(Value value) {
-        if (!domain.contains(value))
-            return null;
-
-        return afterSets.getOrDefault(value, new HashSet<>());
+    public HashSet<Value> getUnconstrainedValues(Value value) {
+        return getAfterSet(value);
     }
 }
